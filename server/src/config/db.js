7b5813d572a0cc4f27505d524ee209350252dadc
Sivam Manoj <manom8193@gmail.com>
2024-08-17 21:50:17 +0530

    dotenv to backend

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 export const Db = async () => {
-  const port = process.env.MONGO_URL || "";
+  const port = process.env.MONGO_URL;
   try {
     await mongoose.connect(port);
     console.log("database connected succesfully");
